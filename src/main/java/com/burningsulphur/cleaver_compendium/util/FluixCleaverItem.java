cc527e6c1650f87eba8435f5b992765845c2f83f
BurningSulphur <119114567+BurningSulphur@users.noreply.github.com>
2025-06-16 00:42:07 +0100

    Fixed fluix issue

--- src/main/java/com/burningsulphur/cleaver_compendium/util/FluixCleaverItem.java
+++ src/main/java/com/burningsulphur/cleaver_compendium/util/FluixCleaverItem.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import appeng.items.tools.fluix.FluixToolType;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.fml.common.Mod;
 import net.yirmiri.dungeonsdelight.common.item.CleaverItem;
 import org.jetbrains.annotations.Nullable;
 
@@ -19,8 +20,33 @@ import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
 
 import appeng.core.localization.GuiText;
-import appeng.hooks.IntrinsicEnchantItem;
+//import appeng.hooks.IntrinsicEnchantItem;
 
+
+public class FluixCleaverItem extends CleaverItem{
+
+    public FluixCleaverItem(Properties props) {
+        super(1.5F, FluixToolType.FLUIX.getToolTier(), 3.4F, -3.0F, props);
+    }
+
+    public int getIntrinsicEnchantLevel(ItemStack stack, Enchantment enchantment) {
+        return enchantment == Enchantments.MOB_LOOTING ? 1 : 0;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltipComponents,
+                                TooltipFlag isAdvanced) {
+        tooltipComponents.add(GuiText.IntrinsicEnchant.text(Enchantments.MOB_LOOTING.getFullname(1)));
+    }
+
+    @Override
+    public boolean isFoil(ItemStack stack) {
+        return true;
+    }
+}
+
+
+/*
 public class FluixCleaverItem extends CleaverItem implements IntrinsicEnchantItem {
     public FluixCleaverItem(Properties props) {
         super(1.5F, FluixToolType.FLUIX.getToolTier(), 3.4F, -3.0F, props);
@@ -42,3 +68,5 @@ public class FluixCleaverItem extends CleaverItem implements IntrinsicEnchantIte
         return true;
     }
 }
+
+ */
